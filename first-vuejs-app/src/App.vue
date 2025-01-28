2896e202916c38b35b8f10be94b9f5eb2a0a841c
Gauthier <g.p220498@gmail.com>
2025-01-28 18:41:11 +0100

    first test

---
+++ first-vuejs-app/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+	import HelloWorld from './components/HelloWorld.vue';
+	import {watch, computed, ref} from 'vue';
+
+	// const sentence = computed( () => {
+	// 	return `Salut je m'appelle ${name.value} et j'ai ${age.value} ans.`;
+	// });
+
+	const name = ref('gpeyre');
+	const age = ref(26)
+
+	// const user = ref({
+	// 	name: 'gpeyre',
+	// 	age: 26
+	// });
+
+	// watch(name, (newValue, oldValue) => {
+	// 	console.log(newValue + ' ' + oldValue)
+	// });
+</script>
+
+<template>
+	<!-- <HelloWorld :user="user"/> -->
+	<!-- <HelloWorld>
+		<template v-slot:name> {{ user.name }} </template>
+		<template v-slot:age> {{ user.age }} </template>
+	</HelloWorld> -->
+	<HelloWorld/>
+	<!-- <div class="bold" :class="[ user.age > 18 ? 'text-green' : 'text-red']">{{ user.name }}</div> -->
+	<!-- <span>{{ sentence }}</span> -->
+	<!-- <span>Salut je m'appelle {{user.name}} et j'ai {{user.age}} ans.</span> -->
+	<br>
+	<br>
+	<!-- <input type="text" v-model="name">
+	<input type="text" v-model="age"> -->
+</template>
+
+<style scoped>
+
+	.bold {
+		font-weight: bolder;
+	}
+
+	.text-red {
+		color:indianred;
+	}
+
+	.text-green {
+		color:green;
+	}
+
+</style>
---
+++ first-vuejs-app/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+	import {ref} from 'vue';
+	// const props = defineProps(['user'])
+	const name = ref('');
+	const emit = defineEmits(['updateName']);
+
+	const handleUpdate = () => {
+
+	}
+</script>
+
+<template>
+	<h1>Hello World</h1>
+	<!-- <p>{{ props.user.name }} {{ props.user.age }}</p> -->
+ 	<!-- <slot name="name"></slot>
+	<slot name="age"></slot> -->\
+
+	{{ name }}
+	<br>
+	<br>
+	<input type="text" v-model="name" @input="handleUpdate">
+</template>
